52e6ca84cb29069ae4b94d4109e12475c712160c
Zebedeusz <michalgil@vp.pl>
2016-10-16 22:43:25 +0200

    deleted DataDividor, added division in Acquisitor

--- src/classification/DataAcquisitor.java
+++ src/classification/DataAcquisitor.java
@@ -1,93 +1,202 @@
 package classification;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 public class DataAcquisitor 
 {
-	int attributesQuantity = 0;
-	int classesQuantity = 0;
-	String dataFileName;
+	private int attributesQuantity = 0;
+	private int classesQuantity = 0;
+	private String dataFileName;
 	
-	public DataAcquisitor(String dataFileName, int attributesQuantity, int classesQuantity)
-	{
-		this.dataFileName = dataFileName;
-		this.attributesQuantity = attributesQuantity;
-		this.classesQuantity = classesQuantity;
-	}
-	
-    String[] example = new String[attributesQuantity+1];
-    List <List> classes = new ArrayList<>();
-  
-    public void runProcedure()
+    private String[] example = new String[attributesQuantity+1];
+    private List <List<String[]>> classes = new ArrayList<>();
+    private List <List<List<String[]>>> dividedData = new ArrayList<>();
+    
+    
+    
+    public void loadData()
     {
+    	getInfoFromUser();
+		
     	try 
     	{
-			getData(dataFileName);
+    		while (!getDataFromFile(dataFileName))
+    		{
+    			this.dataFileName = getStringFromUser("path to data file");
+    		}
+			
+			int i = 0;
+			for (List al : classes)
+			{
+				System.out.println("Size of class" + i + ": " + al.size());
+				i++;
+			}
+			
+			System.out.println("Data aquisition finished successfuly.");
 		} 
     	catch (IOException e) 
     	{
 			e.printStackTrace();
 		}
-    	finally
+    }
+    
+    private boolean getDataFromFile(String dataFileName) throws IOException 
+    {
+    	BufferedReader dataFileReader = null;
+    	
+    	try
     	{
-    		System.out.println("Data aquisition finished successfuly");
+    		dataFileReader = new BufferedReader(new FileReader(dataFileName));
+    		
+        	if (!classes.isEmpty())
+        		classes.removeAll(classes);
+        	
+        	String[] classValues = new String[classesQuantity];
+        	
+        	for (int i = 0; i < classesQuantity; i++)
+        	{
+        		classes.add(new ArrayList<String[]>());
+        		classValues[i] = getStringFromUser("value of class" + i);
+        	}
+        	
+        	while (dataFileReader.ready())
+        	{
+        		example = dataFileReader.readLine().split(",");
+        		
+        		for (int i = 0; i < classValues.length; i++)
+        		{
+        			if (classValues[i].equals(example[attributesQuantity]))
+        			{
+        				classes.get(i).add(example);
+        				break;
+        			}
+        		}
+        	}
+        	
+        	dataFileReader.close();
+        	return true;
+    	}
+    	catch (FileNotFoundException e)
+    	{
+    		System.out.println("The file does not exist in this path.");
+    		return false;
     	}
     }
     
-    private void getData(String dataFileName) throws IOException
+    public void divideData(int chunks)
     {
-    	BufferedReader dataFileReader = new BufferedReader(new FileReader(dataFileName));
-    	String[] classValues = new String[classesQuantity];
-    	
-    	for (int i = 0; i < classesQuantity; i++)
+		List <List<String[]>> tempListClasses = new ArrayList<>();
+		List <String[]> tempListExamples = new ArrayList<>();
+		int dividedClassSize;
+		
+		
+    	for (int i = 0; i < chunks; i++)
     	{
-    		classes.add(new ArrayList<String[]>());
-    		classValues[i] = getValueFromUser("value of class" + i);
+    		if (!tempListClasses.isEmpty())
+    			tempListClasses = new ArrayList<>();
+    		if (!tempListExamples.isEmpty())
+    			tempListExamples = new ArrayList<>();
+    		
+    		for (Iterator<List<String[]>> iterator = classes.iterator(); iterator.hasNext();) 
+    		{
+				List <String[]> iterClass = iterator.next();
+				dividedClassSize = iterClass.size()/chunks;
+				tempListExamples = iterClass.subList(dividedClassSize*i, dividedClassSize*(i+1));
+				tempListClasses.add(tempListExamples);
+			}
+    		
+    		dividedData.add(tempListClasses);
     	}
     	
-    	while (dataFileReader.ready())
+    	for (Iterator<List<String[]>> iterator = classes.iterator(); iterator.hasNext();)
     	{
-    		example = dataFileReader.readLine().split(".");
-    		
-    		for (int i = 0; i < classValues.length; i++)
+    		List<String[]> tempClasses = iterator.next();
+    		int j = 0;
+    		if(tempClasses.size()%chunks!=0)
     		{
-    			if (classValues[i].equals(example[attributesQuantity]))
+    			for(int i = 0; i < tempClasses.size()%chunks; i++)
     			{
-    				classes.get(i).add(example);
-    				System.out.println(classes.get(i));
-    				break;
+    				dividedData.get(i).get(j).add(tempClasses.get(tempClasses.size()-1-i));
     			}
     		}
+    		j++;
     	}
     	
-    	dataFileReader.close();
+    	System.out.println("chunks in divedData: " + dividedData.size());
+    	int j = 0;
+    	for (Iterator<List<List<String[]>>> iterator = dividedData.iterator(); iterator.hasNext();)
+    	{
+    		List <List<String[]>> tempClasses = iterator.next();
+    		int nc = tempClasses.size();
+    		System.out.println("classes in chunk" + j + ": " + nc);
+    		for(int i = 0; i < nc; i++)
+    		{
+    			System.out.println("examples in class" + i + ": " + tempClasses.get(i).size());
+    		}
+    		j++;
+    		
+    	}    	
     }
     
-    private String getValueFromUser(String valueName)
+    
+    
+    
+    
+    
+    
+    public List<List<String[]>> getData()
     {
-        Scanner sc = new Scanner(System.in);
+    	return this.classes;
+    }
+     
+    private void getInfoFromUser()
+    {
+    	dataFileName = getStringFromUser("path to data file");
+    	
+    	while(attributesQuantity == 0)
+    		attributesQuantity = getValueFromUser("quantity of attributes");
+    	
+    	while(classesQuantity == 0)
+    		classesQuantity = getValueFromUser("quantity of classes");
+    }
+    
+    public int getValueFromUser(String valueName)
+    {
+    	Scanner sc = new Scanner(System.in);
+    	int value = 0;
+        System.out.print("Enter " + valueName + ": ");
         
-        System.out.print("Enter " + valueName + ": ");        
-        String value = sc.next();
-        
-        while (value.equals("/s+"))
+        try
         {
-        	System.out.println("Insufficient value.");
-        	getValueFromUser(valueName);
+            value = sc.nextInt();
         }
-        
-        System.out.println("");
-        
-        sc.close();
+
+        catch(InputMismatchException e)
+        {
+        	System.out.println("Unaccepted data type inserted");
+        	value = 0;
+        }
         
         return value;
     }
     
-    
+    public String getStringFromUser(String valueName)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter " + valueName + ": ");
+        
+        String value = sc.next();
+        
+        return value;
+    }
 
 }
--- src/classification/Initialiser.java
+++ src/classification/Initialiser.java
@@ -7,41 +7,11 @@ public class Initialiser {
 
     public static void main(String[] args) throws FileNotFoundException 
     {
-        String dataFileName = args[0];
-        final int attributesQuantity = getValueFromUser("quantity of attributes");
-        final int classesQuantity = getValueFromUser("quantity of classes");
-        
-        while ((dataFileName == "") || (dataFileName.split(".")[1] != "txt"))
-        {
-        	System.out.println("Insufficient data file.");
-        	dataFileName = getStringFromUser("name of file with data");
-        }
-        
-        DataAcquisitor acq = new DataAcquisitor(dataFileName, attributesQuantity, classesQuantity);
+        DataAcquisitor acq = new DataAcquisitor();
+        acq.loadData();
+        acq.divideData(10);
     }
     
-    public static int getValueFromUser(String valueName)
-    {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter " + valueName + ": ");
-        
-        int value = Integer.parseInt(sc.next());
-        sc.close();
-        
-        return value;
-    }
-    
-    public static String getStringFromUser(String valueName)
-    {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Enter " + valueName + ": ");
-        
-        String value = sc.next();
-        sc.close();
-        
-        return value;
-    }
+
     
 }
